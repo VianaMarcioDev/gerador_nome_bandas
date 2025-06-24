e572d82345d31dea9df1ee1c42e3780e7f05ddf4
Márcio Viana <marcioviana.developer@gmail.com>
2025-06-24 18:20:00 -0300

    criar app gerador de nome de bandas

---
+++ gerador_nome_bandas/gerador_nome_bandas.py
@@ -0,0 +1,4 @@
+print("Bem-vindo ao gerador de nomes de bandas!")
+user_city = input("Digite o nome da cidade onde você nasceu: ")
+user_pet_name = input("Digite o nome do seu animal de estimação: ")
+print("O nome sugerido para sua banda é " + user_city + " " + user_pet_name)
